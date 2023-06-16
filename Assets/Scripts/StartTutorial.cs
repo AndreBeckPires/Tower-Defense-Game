6a805ec99588c26a31e327e3f1c2265184888d4f
unknown <andrebeckpires@gmail.com>
2023-06-16 20:20:47 -0300

    ui tutorial

---
+++ Assets/Scripts/StartTutorial.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+
+public class StartTutorial : MonoBehaviour
+{
+    public GameObject ui;
+    public TMP_Text TMPText;
+    [TextArea(3, 10)]
+    public string HOVERTEXT;
+    // Start is called before the first frame update
+    void Start()
+    {
+        TMPText.text = HOVERTEXT;
+    }
+
+    // Update is called once per frame
+    public void close()
+    {
+        ui.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (ui.activeSelf)
+        {
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Time.timeScale = 1.0f;
+        }
+    }
+}
